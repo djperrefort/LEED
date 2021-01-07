0f541cf1b9b61e1a9a465f3003fa3694053b1b69
Daniel <djperrefort@gmail.com>
2021-01-07 11:58:57 -0500

    Adds custom exceptions

---
+++ leed/analysis/exceptions.py
@@ -0,0 +1,9 @@
+"""The ``exceptions`` module defines custom Python exceptions."""
+
+
+class FeatureNotObserved(Exception):
+    """Feature was not observed or does not span indicated wavelength range"""
+
+
+class SamplingRangeError(Exception):
+    """Resampling process extends beyond available wavelength range"""
